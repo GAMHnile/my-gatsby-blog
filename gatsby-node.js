4d6744aff24dd4d57a29066a137da08d8897dd4b
GAMHnile <georgeamhuweh@gmail.com>
2021-08-28 12:33:03 +0100

    updated date in index.js to use local format

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,47 +5,48 @@
  */
 
 // You can delete this file if you're not using it
-const {createFilePath} = require(`gatsby-source-filesystem`);
-const path = require('path');
+const { createFilePath } = require(`gatsby-source-filesystem`)
+const path = require("path")
 
-exports.onCreateNode = ({node, getNode, actions})=>{
-    const {createNodeField} = actions;
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
 
-    if(node.internal.type=== `MarkdownRemark`){
-        const slug = createFilePath({node, getNode});
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode })
+    //basePath can be added optionally here
 
-        createNodeField({
-            node,
-            name: `slug`,
-            value: slug
-        })
-    }
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
 }
 
-exports.createPages = ({graphql, actions})=>{
-    const {createPage} = actions;
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
 
-    return graphql(`
+  return graphql(`
     {
-        allMarkdownRemark {
-          edges {
-            node {
-              fields {
-                slug
-              }
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
             }
           }
         }
-      }  
-    `).then(result=>{
-        result.data.allMarkdownRemark.edges.forEach(({node})=>{
-            createPage({
-                path: node.fields.slug,
-                component: path.resolve(`./src/template/blog-posts.js`),
-                context:{
-                    slug: node.fields.slug
-                }
-            })
-        })
+      }
+    }
+  `).then(result => {
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve(`./src/template/blog-posts.js`),
+        context: {
+          slug: node.fields.slug,
+        },
+      })
     })
+  })
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,62 +1,60 @@
-import React from "react";
-import { Link, graphql } from "gatsby";
-import styled from 'styled-components';
+import React from "react"
+import { Link, graphql } from "gatsby"
+import styled from "styled-components"
 
-import Layout from "../components/layout";
-import SEO from "../components/seo";
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 const LinkStyled = styled(Link)`
   text-decoration: none;
 `
 const BigTitle = styled.h3`
   margin-bottom: 20px;
-`;
+`
 
-export default  ({data}) => {
+export default ({ data }) => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div>
+        <h1>Welcome to George's Blog</h1>
+        <h4>{`The total number of posts is ${data.allMarkdownRemark.totalCount}`}</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <LinkStyled to={node.fields.slug}>
+              <BigTitle>
+                {node.frontmatter.title} -{" "}
+                {new Date(node.frontmatter.date).toLocaleDateString()}
+              </BigTitle>
+            </LinkStyled>
+            <p>{node.excerpt}</p>
+          </div>
+        ))}
+      </div>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </Layout>
+  )
+}
 
-  return(
-  <Layout>
-    <SEO title="Home" />
-    <div>
-    <h1>Welcome to George's Blog</h1>
-    <h4>{`The total number of posts is ${data.allMarkdownRemark.totalCount}`}</h4>
-    {
-      data.allMarkdownRemark.edges.map(({node})=>(
-        <div key={node.id}>
-        <LinkStyled to={node.fields.slug} >
-          <BigTitle>{node.frontmatter.title} - {node.frontmatter.date}</BigTitle>
-        </LinkStyled>
-          <p>{node.excerpt}</p>
-        </div>
-      ))
-    }
-    </div>
-
-
-
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)}
-
-export const data = graphql` 
-query {
-  allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
-    edges {
-      node {
-        fields{
-          slug
-        }
-        id
-        excerpt
-        frontmatter {
-          date
-          description
-          title
+export const data = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          id
+          excerpt
+          frontmatter {
+            date
+            description
+            title
+          }
         }
       }
+      totalCount
     }
-    totalCount
   }
-}
-`;
+`
